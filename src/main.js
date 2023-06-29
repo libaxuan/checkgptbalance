ae33f1b429b37a268cd1c91dafb79ba739b2c727
libaxuan <1107590384@qq.com>
2023-06-29 15:39:52 +0800

    百度统计

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ import App from './App.vue'
 import './styles/global.css'
 
 createApp(App).mount('#app')
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?e4b6b90570dfa8343fa9c6b3490ced79";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
